7a2bb2bb438462a97deb0fe48e1fc0883d720fa4
GAL_son <pancerek1@gmail.com>
2023-05-12 11:07:46 +0200

    lab9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import Posts from './components/posts';
+import Navbar from './components/navbar';
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-      <Posts/>
-    </div>
+    <div className="container-fluid">
+       <Navbar />
+       <div className="container">
+        <Outlet />
+       </div>
+     </div>
   );
 }
 
